021ba4e01444315131b8e2573b87a3ddd2178b98
danagm <dana.gomez@alumnos.udg.mx>
2020-09-25 19:34:01 -0500

    dias vividos

--- holamundo.py
+++ holamundo.py
@@ -3,3 +3,5 @@ nombre = input("cual es tu nombre? ")
 print("hola", nombre)
 edad = int(input("cual es tu edad? "))
 print("estás por cumplir", edad+1)
+dias = edad*365
+print(nombre, "has vivido", dias, "días en toda tu vida")
